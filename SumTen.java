8aa535edf130e5194c38859ef85e7837472c75e4
Divyanshu Singhal <96660033+divyanshu9821@users.noreply.github.com>
2023-09-29 12:12:59 +0530

    Update SumTen.java

--- SumTen.java
+++ SumTen.java
@@ -1,3 +1,6 @@
+// WAP to find sum of 10 integers and print the sum with a proper SOP statement.
+// using for loop
+    
 public class SumTen{
     public static void main(String[] args){
         int arr[] = new int[]{1,2,3,4,5};
